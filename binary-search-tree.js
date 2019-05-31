1a4c520be22ea91adce08981d115e80aa9624bcf
Nadav Kaner <nadavkaner@invisionapp.com>
2019-05-31 09:30:24 +0300

    Add binary search tree skeleton class

---
+++ binary-search-tree.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(value) {
+    this.right = null;
+    this.left = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert() {
+
+  }
+
+  lookup() {
+
+  }
+
+  remove() {
+
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
