22980b978f69debd3b181a3c6ea1a5ae4d706d12
Nadav Kaner <nadavkaner@invisionapp.com>
2019-05-31 20:01:57 +0300

    Add BST insert and stringify

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,8 +1,8 @@
 class Node {
   constructor(value) {
+    this.value = value;
     this.right = null;
     this.left = null;
-    this.value = value;
   }
 }
 
@@ -10,17 +10,39 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  
-  insert() {
 
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
   }
 
-  lookup() {
+  lookup() {}
 
-  }
+  remove() {}
 
-  remove() {
-
+  stringify() {
+    return JSON.stringify(this.root, null, 2);
   }
 }
 
@@ -32,3 +54,4 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+console.log(tree.stringify());
