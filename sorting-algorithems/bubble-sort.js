2277fbba4274422e54b7355bd54a4088090a0b74
Nadav Kaner <nadavkaner1@gmail.com>
2019-06-25 19:36:46 +0300

    Add selection sort algorithem

--- sorting-algorithems/bubble-sort.js
+++ sorting-algorithems/bubble-sort.js
@@ -1,17 +1,30 @@
+function simpleBubbleSort(array) {
+  let length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (array[j] < array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+
+  return array;
+}
+
 function bubbleSortWithWhile(array) {
   let swapped = false;
 
   do {
     swapped = false;
-    
+
     array.forEach((item, index) => {
       if (item > array[index + 1]) {
-        [array[index], array[index + 1]] = [array[index + 1], array[index]]
+        [array[index], array[index + 1]] = [array[index + 1], array[index]];
 
         swapped = true;
       }
-    })
-  } while(swapped)
+    });
+  } while (swapped);
 
   return array;
 }
@@ -22,7 +35,7 @@ function bubbleSortWithSimpleFor(array) {
   let length = array.length;
   for (let i = 0; i < length; i++) {
     swapped = false;
-    
+
     for (let j = 0; j < length - i - 1; j++) {
       if (copy[j] > copy[j + 1]) {
         [copy[j], copy[j + 1]] = [copy[j + 1], copy[j]];
@@ -39,9 +52,8 @@ function bubbleSortWithSimpleFor(array) {
 
 exports.bubbleSort = bubbleSortWithWhile;
 
-
 // USAGE:
 
-const arr = [7,3,1,5,10,4]
+const arr = [7, 3, 1, 5, 10, 4];
 
 console.log(bubbleSortWithSimpleFor(arr));
---
+++ sorting-algorithems/selection-sort.js
@@ -0,0 +1,21 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+
+    if (min !== i) {
+      const temp = array[min];
+      array[min] = array[i];
+      array[i] = temp;
+    }
+  }
+}
+
+selectionSort(numbers);
+console.log(numbers);
